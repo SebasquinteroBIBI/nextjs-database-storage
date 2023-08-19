3869a8559f02833d642cc227a273b4c2c79fe5db
Johan Quintero <sebastian.quintero@bibisoft.com.co>
2023-08-19 09:05:36 -0500

    test route

--- src/app/contact/create/page.jsx
+++ src/app/contact/create/page.jsx
@@ -39,7 +39,7 @@ export default function Page () {
               <label className="mb-3 block text-base font-medium text-[#07074D]">
                 Nombre
               </label>
-              <input type="text" name="firstname" id="firstname" placeholder="Nombre" requiredclassName="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+              <input type="text" name="firstname" id="firstname" required placeholder="Nombre" className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
             </div>
             <div className="mb-5">
               <label className="mb-3 block text-base font-medium text-[#07074D]">
--- src/components/contact/TableList.jsx
+++ src/components/contact/TableList.jsx
@@ -38,7 +38,7 @@ export default function TableList ({ data }) {
             </svg>
           </div>
           <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-            <Link className="bg-green-500 text-white active:bg-green-800 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" href="http://localhost:3000/contact/create">CREAR CONTACTO</Link>
+            <Link className="bg-green-500 text-white active:bg-green-800 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" href="contact/create">CREAR CONTACTO</Link>
           </div>
         </div>
       </div>
@@ -82,7 +82,7 @@ export default function TableList ({ data }) {
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                         </svg>
                       </div> */}
-                    <Link href={`http://localhost:3000/contact/edit/${contact.id}`} className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                    <Link href={`contact/edit/${contact.id}`} className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                       </svg>
